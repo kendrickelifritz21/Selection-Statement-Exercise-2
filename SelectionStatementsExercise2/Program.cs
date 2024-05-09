ec0e6b0df74e0169f3f056761652e2f1407018a9
Elifritz <Kendrick_Elifritz@Dell.com>
2024-05-08 17:42:44 -0700

    Completed switch statement exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What's your favorite school subject?");
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is a hard subject!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is an intriguing subject!");
+                    break;
+                case "History":
+                    Console.WriteLine("History is an important subject!");
+                    break;
+                case "English":
+                    Console.WriteLine("English is a fun subject!");
+                    break;
+                case "Art":
+                    Console.WriteLine("Art is a creative subject!");
+                    break;
+                default:
+                    Console.WriteLine("That's a great subject!");
+                    break;
+            }
         }
     }
 }
